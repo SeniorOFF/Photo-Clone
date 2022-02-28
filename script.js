f00bfb61c2c62beef2b2e9ded28eaaa536eb835c
SeniorOFF <ssenioroff@gmail.com>
2022-02-28 21:02:22 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var modal = document.getElementById("modal");
+
+function soz(){
+    modal.style.transform = "translateX(0px)"
+}
+function chiqish(){
+    modal.style.transform = "translateX(400px)"
+}
+
+function kun(){
+    var element = document.body;
+    element.classList.toggle("tun")
+}
